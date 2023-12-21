574d0db44cb9fc61d2aa25127d6956b2d98d06c3
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-12-21 14:24:16 +0000

    allow null admin consent

--- local.tf
+++ local.tf
@@ -5,6 +5,6 @@ locals {
         service_principal_name = admin_consent.service_principal_name
         app_role_id            = admin_consent.app_role_id
         resource_object_id     = admin_consent.resource_object_id
-      }
-  ]]))
+      } if service_principal.admin_consents != null
+  ] if var.service_principals != null]))
 }
